5ec24e05e9d6455128621fba117d939fb63fbecb
Tín Huỳnh <huynhchitin28071995@live.com>
2021-03-23 23:32:14 +0700

    Finished problem 7

--- DataStructure-Algorithm/src/linkedList/Problems.java
+++ DataStructure-Algorithm/src/linkedList/Problems.java
@@ -64,6 +64,74 @@ public class Problems {
 		System.out.println(n + "-th value from the end of the list = " + curNode.getData());
 	}
 
+	private static void problem5(LinkedList list, int n) { // solve with 1 scan
+		if (n < 1)
+			return;
+//		ListNode pNthNode = null;
+		ListNode pNthNode = list.getHead();
+		ListNode pTemp = list.getHead();
+		for (int i = 0; i < n; i++) {
+			if (pTemp == null)
+				return;
+			pTemp = pTemp.getNext();
+		}
+		while (pTemp != null) {
+			pTemp = pTemp.getNext();
+//			pNthNode = (pNthNode == null ? list.getHead() : pNthNode.getNext());
+			pNthNode = pNthNode.getNext();
+		}
+		if (pNthNode != null)
+			System.out.println(n + "-th value from the end of the list = " + pNthNode.getData());
+	}
+
+	private static int count = 0;
+
+	private static void problem6(ListNode head, int n) { // solve problem 5 with recursion
+		if (n < 1)
+			return;
+//		if (head == null) {
+//			System.out.println("Fewer number of nodes in the list.");
+//			return;
+//		}
+		if (head != null) { // always run until head == null
+			problem6(head.getNext(), n);
+			count++;
+			if (n == count) {
+				System.out.println(n + "-th value from the end of the list = " + head.getData());
+				return;
+			}
+		}
+
+	}
+
+	private static void problem7(LinkedList list) { // check linked list is null terminated or cyclic - self-solved
+		ListNode curNode = list.getHead();
+		while (curNode != null) {
+			curNode = curNode.getNext();
+			if (curNode == list.getHead()) {
+				System.out.println("This linked list is cyclic!");
+				return;
+			}
+		}
+		System.out.println("This linked list is null-terminated!");
+	}
+
+	private static boolean problem7a(ListNode node, int listSize) { // find other nodes that has the same next pointer
+		if (node == null)
+			return false;
+		ListNode temp = node.getNext();
+		int count = 0;
+		while (temp != null && count < listSize) { // the problem appears when this while loop is in a loop of nodes.
+			if (temp.equals(node)) {
+				System.out.println("This linked list is cyclic!");
+				return true;
+			}
+			temp = temp.getNext();
+			count++;
+		}
+		return problem7a(node.getNext(), listSize);
+	}
+
 	private static int count(ListNode curNode) {
 		int count = 0;
 //		ListNode temp = curNode.getNext();
@@ -80,29 +148,52 @@ public class Problems {
 			ll.insertAtBegin(new ListNode(i));
 		}
 		ListNode node = ll.getHead();
+		ListNode temp = node;
 		while (node != null) {
 			System.out.println(node.getData());
 			node = node.getNext();
+			if (node != null) {
+				temp = node;
+			}
 		}
-		for (int i = 0; i < 10; i++) {
-			problem2(ll, i);
-		}
-		problem2(ll, 10);
-		problem2(ll, 11);
-		problem2(ll, 13);
+//		for (int i = 0; i < 10; i++) {
+//			problem2(ll, i);
+//		}
+//		problem2(ll, 10);
+//		problem2(ll, 11);
+//		problem2(ll, 13);
+//
+//		for (int i = 0; i < 10; i++) {
+//			problem3(ll, i);
+//		}
+//		problem3(ll, 10);
+//		problem3(ll, 11);
+//		problem3(ll, 13);
+//
+//		for (int i = 0; i < 10; i++) {
+//			problem4(ll, i);
+//		}
+//		problem4(ll, 10);
+//		problem4(ll, 11);
+//		problem4(ll, 13);
 
-		for (int i = 0; i < 10; i++) {
-			problem3(ll, i);
-		}
-		problem3(ll, 10);
-		problem3(ll, 11);
-		problem3(ll, 13);
+//		for (int i = 0; i < 10; i++) {
+//			problem5(ll, i);
+//		}
+//		problem5(ll, 10);
+//		problem5(ll, 11);
+//		problem5(ll, 13);
 
-		for (int i = 0; i < 10; i++) {
-			problem4(ll, i);
-		}
-		problem4(ll, 10);
-		problem4(ll, 11);
-		problem4(ll, 13);
+//		for (int i = 0; i < 10; i++) {
+//			problem6(ll.getHead(), i);
+//			count = 0;
+//		}
+//		problem6(ll.getHead(), 10);
+//		problem6(ll.getHead(), 11);
+//		problem6(ll.getHead(), 13);
+
+		System.out.println("IsCyclic = " + problem7a(ll.getHead(), ll.length()));
+		temp.setNext(ll.getHead().getNext().getNext());
+		System.out.println("IsCyclic = " + problem7a(ll.getHead(), ll.length()));
 	}
 }
